2ee0f3372fdb565d10a576a3862007a1656bc34b
Esteban Flores <esflores@microsoft.com>
2023-07-04 22:00:20 -0600

    cambia varios archivos para agregar funcionalidad

---
+++ js/login.js
@@ -0,0 +1,28 @@
+const usuario = document.getElementById("username");
+const password = document.getElementById("password");
+
+function handleLogin(login){
+  document.cookie = `usuario=${login.usuario}`
+  console.log(login)
+  return true;
+}
+
+function handleFormSubmit(e) {
+  e.preventDefault();
+  const usuarioValue = usuario.value;
+  const passwordValue = password.value;
+  const login = {
+    usuario: usuarioValue,
+    password: passwordValue,
+  };
+
+  try{
+    const session = handleLogin(login);
+    if (session && document.cookie.includes("usuario")){
+      location.href = "/cuenta/perfil.html"
+    }
+
+  } catch (error){
+    console.log(error)
+  }
+}
