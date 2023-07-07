24969861f2e2400687687415fe910cdf23051ce0
Carlos Montero <cmonterom@ucenfotec.ac.cr>
2023-07-06 20:24:13 -0600

    agrega pagina de agregar puesto y su .js

---
+++ js/agregarpuesto.js
@@ -0,0 +1,20 @@
+const slider1 = document.getElementById("slider1");
+const slider2 = document.getElementById("slider2");
+const value1 = document.getElementById("value1");
+const value2 = document.getElementById("value2")
+
+slider1.addEventListener("input", function() {
+  value1.textContent = slider1.value;
+  if (parseInt(slider1.value) > parseInt(slider2.value)) {
+    slider2.value = slider1.value;
+    value2.textContent = slider1.value;
+  }
+});
+
+slider2.addEventListener("input", function() {
+  value2.textContent = slider2.value;
+  if (parseInt(slider2.value) < parseInt(slider1.value)) {
+    slider1.value = slider2.value;
+    value1.textContent = slider2.value;
+  }
+});
