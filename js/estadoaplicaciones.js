30924124d9449de5d2b1a6311c449b3a8b873fc3
Esteban Flores <esflores@microsoft.com>
2023-07-13 17:45:57 -0600

    cambia formato

--- js/estadoaplicaciones.js
+++ js/estadoaplicaciones.js
@@ -91,7 +91,6 @@ function getJobApplications(userId) {
 }
 
 function filtrarAplicaciones() {
-
   mainContent.innerHTML = "<h1>Mis aplicaciones</h1>";
 
   function filtrarEstado(item) {
@@ -107,10 +106,10 @@ function filtrarAplicaciones() {
     aplicacionDiv.id = `puesto-${index}`;
 
     aplicacionDiv.innerHTML = `
-      <div id="puesto-svg-${index}">
-      <img class="img-style" src="${puesto.companyImg}" alt="imagen-empresa">
+    <div id="puesto-svg-${index}">
+      <img class="img-style" src="${puesto.companyImg}" alt="imagen-empresa" />
       </div>
-    <div style="width:75%">
+    <div style="width: 75%">
       <h2>${puesto.title}</h2>
       <div class="flex flex-align-center flex-gap-5">
         <svg
@@ -133,8 +132,9 @@ function filtrarAplicaciones() {
       </div>
       <p class="notification-description">${puesto.description}</p>
     </div>
-    <h3 class="flex" style="align-items: center; text-align: right">${puesto.status}</h3>
-    
+    <h3 class="flex" style="align-items: center; text-align: right">
+      ${puesto.status}
+    </h3> 
     `;
 
     mainContent.appendChild(aplicacionDiv);
@@ -150,36 +150,38 @@ function renderApplications() {
     aplicacionDiv.id = `puesto-${index}`;
 
     aplicacionDiv.innerHTML = `
-      <div id="puesto-svg-${index}">
-      <img class="img-style" src="${aplicacion.companyImg}" alt="imagen-empresa">
-      </div>
-    <div style="width:75%">
-      <h2>${aplicacion.title}</h2>
-      <div class="flex flex-align-center flex-gap-5">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-6 h-6"
-          style="height: 20px; width: 20px"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 
-0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5"
-          />
-        </svg>
-    
-        <p>${aplicacion.date}</p>
-      </div>
-      <p class="notification-description">${aplicacion.description}</p>
+    <div id="puesto-svg-${index}">
+      <img class="img-style" src="${aplicacion.companyImg}" alt="imagen-empresa" />
     </div>
-    <h3 class="flex" style="align-items: center; text-align: right">${aplicacion.status}</h3>
-    
-    `;
+    <div style="width: 75%">
+      <h2>${aplicacion.title}</h2>
+    <div class="flex flex-align-center flex-gap-5">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-6 h-6"
+        style="height: 20px; width: 20px"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18  0A2.25 2.25 
+0   005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5"
+        />
+      </svg>
+
+      <p>${aplicacion.date}</p>
+    </div>
+    <p class="notification-description">${aplicacion.description}</p>
+  </div>
+  <h3 class="flex" style="align-items: center; text-align: right">
+  ${aplicacion.status}
+  </h3>
+
+  `;
 
     mainContent.appendChild(aplicacionDiv);
   });
