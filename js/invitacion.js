d994d1a076aba711b34b626ec6df5a7c5aa37256
Esteban Flores <esflores@microsoft.com>
2023-07-13 20:39:39 -0600

    agrega pagina de invitacion individual

--- js/invitacion.js
+++ js/invitacion.js
@@ -1,6 +1,17 @@
-const mainContent = document.getElementById("main-content");
+const invitationTitle = document.getElementById("titulo-invitacion");
+const invitationStatus = document.getElementById("estado-invitacion");
+const invitationDescription = document.getElementById("desc-invitacion");
+const invitationDate = document.getElementById("fecha-invitacion");
+const invitationEmpresa = document.getElementById("empresa-invitacion");
 
-function getInvitations(userId) {
+const urlParams = new URLSearchParams(window.location.search);
+let invitationId = urlParams.get("id");
+
+if (invitationId === null) {
+  location.href = "/invitaciones/";
+}
+
+function getInvitation(invitationId) {
   const invitations = [
     {
       id: 0,
@@ -57,33 +68,16 @@ function getInvitations(userId) {
       status: "Denegada",
     },
   ];
-  return invitations;
+  return invitations[invitationId];
 }
 
-function renderInvitations() {
-  const invitations = getInvitations(0);
-  const invitationsHTML = invitations.map((invitation, index) => {
-    return `
-    <div
-      id="invitacion-${index}"
-      class="padding-box flex flex-align-center flex-gap-10 notification"
-    >
-      <div>
-        <h2>${invitation.title}</h2>
-        <h3>Estado de la aplicacion: ${invitation.status}</h3>
-        <div class="flex flex-align-center flex-gap-5">
-          <p>${invitation.description}</p>
-        </div>
-      </div>
-      <a href="/invitaciones/invitacion.html?id=${invitation.id}">
-        <button class="main-button">Ver invitacion</button>
-      </a>
-    </div>
-
-
-    `;
-  });
-  mainContent.innerHTML = invitationsHTML.join("");
+function renderInvitation() {
+  const invitation = getInvitation(invitationId);
+  invitationTitle.innerText = invitation.title;
+  invitationStatus.innerText = invitation.status;
+  invitationDescription.innerText = invitation.description;
+  invitationDate.innerText = invitation.date;
+  invitationEmpresa.innerText = invitation.empresa;
 }
 
-renderInvitations();
+renderInvitation();
