a09fdf93602a003be7d835fdfef86acb9c46c2f8
Esteban <esfloresicrosoft.com>
2023-07-13 22:24:33 -0600

    Password recovery html and half the javascript

---
+++ js/recoverpassword.js
@@ -0,0 +1,40 @@
+const correo = document.getElementById("email");
+
+function generateRandomId(){
+  return (Math.random() * 0xfffff * 1000000).toString(16).slice(0, 6);
+}
+
+function handleRecover(login){
+  let randomId = generateRandomId();
+  try {
+    const session = {...login, id:randomId};
+    document.cookie = `usuario=${session.correo}; path=/; max-age=3600`;
+    document.cookie = `userId=${session.id}; path=/; max-age=3600`;
+    document.cookie = `userType=endUser; path=/; max-age=3600`
+    return session;
+  } catch(e) {
+    alert("Se produjo un error al recuperar la contraseña, intente nuevamente.");
+    return false;
+  }
+}
+
+function handleFormSubmit(e) {
+  e.preventDefault();
+  const usuarioValue = usuario.value;
+  const passwordValue = password.value;
+  const login = {
+    usuario: usuarioValue,
+    password: passwordValue,
+  };
+
+  try{
+    const session = handleLogin(login);
+    console.log(session)
+    if (session && document.cookie.includes("userId")){
+      location.href = `${document.referrer}`;
+    }
+
+  } catch (error){
+    console.log(error);
+  }
+}
