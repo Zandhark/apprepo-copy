b231cb019cf71b034f468661eb13e6ee71a331b8
Esteban Flores <esflores@microsoft.com>
2023-07-14 09:38:44 -0600

    cambia logica para recuperar contraseña

--- js/recoverpassword.js
+++ js/recoverpassword.js
@@ -1,40 +1,7 @@
 const correo = document.getElementById("email");
 
-function generateRandomId(){
-  return (Math.random() * 0xfffff * 1000000).toString(16).slice(0, 6);
-}
-
-function handleRecover(login){
-  let randomId = generateRandomId();
-  try {
-    const session = {...login, id:randomId};
-    document.cookie = `usuario=${session.correo}; path=/; max-age=3600`;
-    document.cookie = `userId=${session.id}; path=/; max-age=3600`;
-    document.cookie = `userType=endUser; path=/; max-age=3600`
-    return session;
-  } catch(e) {
-    alert("Se produjo un error al recuperar la contraseña, intente nuevamente.");
-    return false;
-  }
-}
-
 function handleFormSubmit(e) {
   e.preventDefault();
-  const usuarioValue = usuario.value;
-  const passwordValue = password.value;
-  const login = {
-    usuario: usuarioValue,
-    password: passwordValue,
-  };
-
-  try{
-    const session = handleLogin(login);
-    console.log(session)
-    if (session && document.cookie.includes("userId")){
-      location.href = `${document.referrer}`;
-    }
-
-  } catch (error){
-    console.log(error);
-  }
+  const correoValue = correo.value;
+  alert("Se ha enviado un correo a " + correoValue + " para recuperar su contraseña");
 }
