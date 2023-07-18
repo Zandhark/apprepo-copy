bb6c4a28a8044a877a1ae033b44401668062582b
Esteban Flores <esflores@microsoft.com>
2023-07-18 14:49:19 -0600

    obtiene puesto del backend

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,16 @@ app.get("/api/puestos", (req, res) => {
   res.json(puestos);
 });
 
+app.get("/api/puestos/:id", (req, res) => {
+  const puestos = getJobs();
+  const puesto = puestos.find((puesto) => puesto.id === parseInt(req.params.id));
+  if (!puesto) {
+    res.status(404).json({ error: "No se encontró el puesto" });
+    return;
+  }
+  res.json(puesto);
+});
+
 app.post("/api/login", (req, res) => {
   const { email, password } = req.body;
   const user = login(email);
