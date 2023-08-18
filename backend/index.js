a15e36961ef581a9635617b3f394426474136773
carlosmonterom <135184290+carlosmonterom@users.noreply.github.com>
2023-08-17 22:28:05 -0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,6 @@ const Session = require("./models/sessionModel.js");
 const Notification = require("./models/notificationModel.js");
 const User = require("./models/userModel.js");
 const Aplication = require("./models/aplicationModel.js");
-const Invitation = require("./models/invitationModel.js");
 
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
